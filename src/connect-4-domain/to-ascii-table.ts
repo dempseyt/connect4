8c8688592b6c497c0783f4e7908d797bebc894c4
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-07 15:14:16 +0800

    GREEN: to-ascii-table given a 1x1 grid with content greater than 1 character in length it returns a 1x1 ascii table

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -1,14 +1,24 @@
 function toAsciiTable(grid: Array<Array<string>>): string {
+  let characterWidth: number = 2
+  const boarderChar: string = '-'
   const table = grid.reduce((tableRows, currentRow) => {
     tableRows.push(
       currentRow.reduce((tableRow, currentElement): string => {
+        characterWidth += currentElement.length
         return tableRow.concat(`| ${currentElement} |`)
       }, ''),
     )
     return tableRows
   }, [])
 
-  return grid.length !== 0 ? ['', '|---|', table[0], '|---|'].join('\n') : ''
+  return grid.length !== 0
+    ? [
+        '',
+        `|${boarderChar.repeat(characterWidth)}|`,
+        table[0],
+        `|${boarderChar.repeat(characterWidth)}|`,
+      ].join('\n')
+    : ''
 }
 
 export default toAsciiTable
