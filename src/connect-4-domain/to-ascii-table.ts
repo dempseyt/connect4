aacad4b8b5d7190c73f00bbdfd9dbd35d343a327
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-13 16:00:12 +0800

    GREEN: to-ascii-table given a grid with 1 row and multiple columns of the same length returns a ascii table with 1 row and multiple columns.

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -1,32 +1,49 @@
-function createBoarder(boarderChar: string, width: number): string {
-  return `|${boarderChar.repeat(width)}|`
+function createBoarder(borderChar: string, cellWidthPerColumn: Array<number>): string {
+  return cellWidthPerColumn.reduce((borderString, width) => {
+    return borderString.concat(borderChar.repeat(width + 2), '|')
+  }, '|')
 }
 
-function defaultResolver<T>(value: T): string {
-  if (value === undefined || value === null) {
-    return ``
-  }
-  return `${value}`
+const defaultResolver = <T>(value: T): string =>
+  value === undefined || value === null ? '' : `${value}`
+
+function getLargestCharacterWidthPerColumn(grid: Array<Array<string>>): Array<number> {
+  return grid.reduce((maxColumnWidths, currentRow) => {
+    return currentRow.reduce((maxColumnWidths, cell, columnIndex) => {
+      if (cell.length > maxColumnWidths[columnIndex]) {
+        maxColumnWidths[columnIndex] = cell.length
+      }
+      return maxColumnWidths
+    }, maxColumnWidths)
+  }, Array(grid[0].length).fill(0))
+}
+
+function resolveGridCells<T>(
+  grid: Array<Array<T>>,
+  cellResolver: (value: T) => string = defaultResolver,
+): Array<Array<string>> {
+  return grid.map((row) => row.map((cell) => cellResolver(cell)))
 }
 
 function toAsciiTable<T>(
   grid: Array<Array<T>>,
   cellResolver: (value: T) => string = defaultResolver,
 ): string {
-  let characterWidth: number = 2
-  const tableRows = grid.reduce((tableRows, currentRow) => {
+  if (grid.length === 0) {
+    return ''
+  }
+  const resolvedGrid = resolveGridCells(grid, cellResolver)
+  const largestCharacterWidthPerColumn = getLargestCharacterWidthPerColumn(resolvedGrid)
+  const tableRows = resolvedGrid.reduce((tableRows, currentRow) => {
     tableRows.push(
       currentRow.reduce((tableRow, currentElement) => {
-        const resolvedElement = cellResolver(currentElement)
-        characterWidth += resolvedElement.length
-        return tableRow.concat(`| ${resolvedElement} |`)
-      }, ''),
+        return tableRow.concat(` ${currentElement} |`)
+      }, '|'),
     )
     return tableRows
   }, [] as Array<string>)
-
-  let boarder: string = createBoarder('-', characterWidth)
-  return grid.length !== 0 ? ['', boarder, tableRows[0], boarder].join('\n') : ''
+  let boarder: string = createBoarder('-', largestCharacterWidthPerColumn)
+  return ['', boarder, tableRows[0], boarder].join('\n')
 }
 
 export default toAsciiTable
