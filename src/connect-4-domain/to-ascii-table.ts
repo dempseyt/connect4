3ca8cf87ab8a92eac417f8f0c7f92745a3424d6d
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-13 14:34:53 +0800

    GREEN: to-ascii-table given a grid with 1 row and 1 column and a custom cell resolver it will use the custom cell resolver to resolve the value of the cell.

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -2,21 +2,28 @@ function createBoarder(boarderChar: string, width: number): string {
   return `|${boarderChar.repeat(width)}|`
 }
 
-function toAsciiTable(grid: Array<Array<string | undefined | null>>): string {
+function defaultResolver<T>(value: T): string {
+  if (value === undefined || value === null) {
+    return ``
+  }
+  return `${value}`
+}
+
+function toAsciiTable<T>(
+  grid: Array<Array<T>>,
+  cellResolver: (value: T) => string = defaultResolver,
+): string {
   let characterWidth: number = 2
   const tableRows = grid.reduce((tableRows, currentRow) => {
     tableRows.push(
-      currentRow.reduce((tableRow: string, currentElement) => {
-        if (currentElement === undefined || currentElement === null) {
-          return tableRow.concat(`|  |`)
-        }
-
-        characterWidth += currentElement.length
-        return tableRow.concat(`| ${currentElement} |`)
+      currentRow.reduce((tableRow, currentElement) => {
+        const resolvedElement = cellResolver(currentElement)
+        characterWidth += resolvedElement.length
+        return tableRow.concat(`| ${resolvedElement} |`)
       }, ''),
     )
     return tableRows
-  }, [])
+  }, [] as Array<string>)
 
   let boarder: string = createBoarder('-', characterWidth)
   return grid.length !== 0 ? ['', boarder, tableRows[0], boarder].join('\n') : ''
