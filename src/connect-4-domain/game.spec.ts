02904b00509dc395f4457d7fd17ae9cc1d2f9a67
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-14 14:46:00 +0800

    RED: new game given defaults returns an instance of Game creates a 6x7 board.

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -1,5 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import GameFactory from '@/connect-4-domain/game'
+import _toAsciiTable from './to-ascii-table'
+import { BoardCell } from '@/connect-4-domain/BoardCell'
+
+function toAsciiTable(board: Array<Array<BoardCell>>): string {
+  const cellResolver = (cell: BoardCell) => (cell.player === undefined ? ` ` : `${cell.player}`)
+  return _toAsciiTable(board, cellResolver)
+}
+
 describe('game', () => {
   describe('new game', () => {
     describe('given defaults', () => {
@@ -7,150 +15,25 @@ describe('game', () => {
         const game = new GameFactory()
         expect(game).toBeInstanceOf(GameFactory)
       })
-      it.skip('creates a 6x7 board', () => {
+      it('creates a 6x7 board', () => {
         const game = new GameFactory()
         const board = game.getBoard()
+        const asciiBoard = toAsciiTable(board)
         expect(asciiBoard).toMatchInlineSnapshot(`
-          [
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-            [
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-              {
-                "player": undefined,
-              },
-            ],
-          ]
+          "
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|
+          |   |   |   |   |   |   |   |
+          |---|---|---|---|---|---|---|"
         `)
       })
     })
