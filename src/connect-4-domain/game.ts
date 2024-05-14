96e39f5611f184b61449be7893b35b56779787e5
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-14 15:31:44 +0800

    GREEN: new game given defaults creates a game where player 1 starts with a number of tokens equal to half the number of cells

--- src/connect-4-domain/game.ts
+++ src/connect-4-domain/game.ts
@@ -5,8 +5,20 @@ export type BoardCell = {
 }
 
 type GameParameters = {
-  boardDimensions: { rows: number; columns: number }
+  boardDimensions: BoardDimensions
 }
+
+type BoardDimensions = {
+  rows: number
+  columns: number
+}
+
+interface PlayerStats {
+  playerNumber: 1 | 2
+  remainingDiscs: number
+}
+
+type PlayerNumber = 1 | 2
 
 interface Game {
   getBoard: () => ReadonlyArray<Array<BoardCell>>
@@ -14,13 +26,19 @@ interface Game {
 
 class GameFactory implements Game {
   private board: Array<Array<BoardCell>>
+  private players: Record<PlayerNumber, PlayerStats>
 
   constructor({ boardDimensions }: GameParameters = { boardDimensions: { rows: 6, columns: 7 } }) {
     this.board = this.#createBoard(boardDimensions)
+    this.players = this.#createPlayers(boardDimensions)
   }
 
   getBoard() {
     return this.board
+  }
+
+  getPlayerStats(playerNumber: 1 | 2): PlayerStats {
+    return this.players[playerNumber]
   }
 
   #createBoard(boardDimensions: { rows: number; columns: number }): Array<Array<BoardCell>> {
@@ -28,6 +46,19 @@ class GameFactory implements Game {
       new Array(boardDimensions.columns).fill(undefined).map(() => ({ player: undefined }))
     const board = new Array(boardDimensions.rows).fill(undefined).map(callback)
     return board
+  }
+
+  #createPlayers(boardDimensions: BoardDimensions): Record<PlayerNumber, PlayerStats> {
+    return {
+      1: {
+        playerNumber: 1,
+        remainingDiscs: (boardDimensions.rows * boardDimensions.columns) / 2,
+      },
+      2: {
+        playerNumber: 2,
+        remainingDiscs: (boardDimensions.rows * boardDimensions.columns) / 2,
+      },
+    }
   }
 }
 
