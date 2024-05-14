a8239ebaf68b9d81ab3e9ff3c01013fee9945701
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-14 14:48:55 +0800

    REFACTOR: new game given defaults returns an instance of Game creates a 6x7 board.

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from 'vitest'
-import GameFactory from '@/connect-4-domain/game'
+import GameFactory, { BoardCell } from '@/connect-4-domain/game'
 import _toAsciiTable from './to-ascii-table'
-import { BoardCell } from '@/connect-4-domain/BoardCell'
 
 function toAsciiTable(board: Array<Array<BoardCell>>): string {
   const cellResolver = (cell: BoardCell) => (cell.player === undefined ? ` ` : `${cell.player}`)
