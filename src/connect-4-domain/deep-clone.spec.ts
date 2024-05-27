cfa366af508e26e570cb7f31158b130ad1969b36
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-27 16:40:16 +0800

    RED: deep-clone should return a deep copy of an array

--- src/connect-4-domain/deep-clone.spec.ts
+++ src/connect-4-domain/deep-clone.spec.ts
@@ -4,4 +4,18 @@ describe('deep-clone', () => {
   it('should return a primitive value as is', () => {
     expect(deepClone(2)).toBe(2)
   })
+  it('should return a deep copy of an array', () => {
+    const original: (object | number[] | number)[] = [{ a: 1 }, 2, [3, 4]]
+    const cloned = deepClone(original)
+    expect(cloned).not.toBe(original)
+    expect(cloned[0]).not.toBe(original[0])
+    expect(cloned[0]).toBeInstanceOf(Object)
+    expect(Object.keys(cloned[0], 'a')).toHaveLength(1)
+    expect(Object.hasOwn(cloned[0], 'a')).toBeTruthy()
+    expect(cloned[0]['a']).toStrictEqual(original[0]['a'])
+    expect(cloned[1]).toBe(original[1])
+    expect(cloned[2]).not.toBe(original[2])
+    expect(cloned[2][0]).toBe(original[2][0])
+    expect(cloned[2][1]).toBe(original[2][1])
+  })
 })
