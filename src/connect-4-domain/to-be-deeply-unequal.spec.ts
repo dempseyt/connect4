4686269eaa28b4ef93bceac3c2fe729df6e0ce85
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-27 15:08:59 +0800

    RED/GREEN: should fail when one object has an additional key, and shares a reference to a value

--- src/connect-4-domain/to-be-deeply-unequal.spec.ts
+++ src/connect-4-domain/to-be-deeply-unequal.spec.ts
@@ -1,19 +1,64 @@
-import { describe, expect, it } from 'vitest'
+import { describe, it, expect } from 'vitest'
 
-describe('to-be-deeply-unequal', () => {
-  it('should fail when given objects are the same objects', () => {
-    const obj1 = { a: 1, b: 2 }
-    const obj2 = obj1
-    expect(obj1).not.toBeDeeplyUnequal(obj2)
+describe('toBeDeeplyUnequal', () => {
+  it('should fail when given objects are the same object', () => {
+    const firstObject = { a: 1, b: 2 }
+    const secondObject = firstObject
+    expect(firstObject).not.toBeDeeplyUnequal(secondObject)
   })
   it('should pass when given objects are different objects', () => {
-    const obj1 = {}
-    const obj2 = {}
-    expect(obj1).toBeDeeplyUnequal(obj2)
+    const firstObject = {}
+    const secondObject = {}
+    expect(firstObject).toBeDeeplyUnequal(secondObject)
   })
-  it('should pass given an array and an object', () => {
+  it('should pass when objects are unequal at a shallow level', () => {
+    const object1 = { a: 1, b: 2 }
+    const object2 = { a: 1, b: 2 }
+    expect(object1).toBeDeeplyUnequal(object2)
+  })
+  it('should pass when one object has an additional key at a shallow level', () => {
+    const object1 = { a: 1, b: 2 }
+    const object2 = { a: 1, b: 2, c: 3 }
+    expect(object1).toBeDeeplyUnequal(object2)
+  })
+  it('should fail when one object has an additional key, and shares a reference to a value', () => {
+    const innerArray = []
+    const object1 = { a: 1, b: innerArray }
+    const object2 = { a: 1, b: innerArray, c: 3 }
+    expect(object1).not.toBeDeeplyUnequal(object2)
+  })
+  it('it should pass, given an object and an array', () => {
     const arr = []
     const obj = {}
-    expect(arr).toBeDeeplyUnequal(obj)
+    expect(obj).toBeDeeplyUnequal(arr)
+  })
+  it('should pass, given null and an object', () => {
+    const obj = {}
+    expect(null).toBeDeeplyUnequal(obj)
+  })
+  it('should pass, given undefined and an object', () => {
+    const obj = {}
+    expect(undefined).toBeDeeplyUnequal(obj)
+  })
+  it('should fail, given two arrays that are the same', () => {
+    const firstArray = []
+    const secondArray = firstArray
+    expect(firstArray).not.toBeDeeplyUnequal(secondArray)
+  })
+  it('should pass given two arrays that are different', () => {
+    const firstArray = []
+    const secondArray = []
+    expect(firstArray).toBeDeeplyUnequal(secondArray)
+  })
+  it('should pass when arrays are unequal at a shallow level', () => {
+    const firstArray = [1, 2]
+    const secondArray = [1, 3]
+    expect(firstArray).toBeDeeplyUnequal(secondArray)
+  })
+  it('should fail when arrays are equal at a shallow level', () => {
+    const innerArray = []
+    const firstArray = [innerArray]
+    const secondArray = [innerArray]
+    expect(firstArray).not.toBeDeeplyUnequal(secondArray)
   })
 })
--- src/connect-4-domain/to-be-deeply-unequal.ts
+++ src/connect-4-domain/to-be-deeply-unequal.ts
@@ -1,8 +1,43 @@
 import { MatcherResult } from '@/vitest'
 
-const toBeDeeplyUnequal = (isNot: boolean, received: object, expected: object): MatcherResult => {
+function checkIsPlainObjectOrArray(value) {
+  return (
+    (value !== null &&
+      typeof value === 'object' &&
+      Object.getPrototypeOf(value) === Object.prototype) ||
+    Array.isArray(value)
+  )
+}
+
+function checkIfDeepUnequal(value1, value2) {
+  if (
+    (checkIsPlainObjectOrArray(value1) && !checkIsPlainObjectOrArray(value2)) ||
+    (!checkIsPlainObjectOrArray(value1) && checkIsPlainObjectOrArray(value2)) ||
+    (!checkIsPlainObjectOrArray(value1) && !checkIsPlainObjectOrArray(value2))
+  ) {
+    return true
+  }
+
+  if (value1 === value2) return false
+
+  const objectKeys1 = Object.keys(value1)
+
+  const isDeepUnequal = objectKeys1.reduce((isDeepUnequal, currentKey) => {
+    return isDeepUnequal && checkIfDeepUnequal(value1[currentKey], value2[currentKey])
+  }, true)
+
+  return isDeepUnequal
+}
+
+function toBeDeeplyUnequal(
+  this: { isNot: boolean } | void,
+  received: object,
+  expected: object,
+): MatcherResult {
+  const isNot = this ?? {}
+  const areObjectsDifferent = checkIfDeepUnequal(received, expected)
   return {
-    pass: received !== expected,
+    pass: areObjectsDifferent,
     message: () => `Objects are deeply ${isNot ? 'un' : ''}equal`,
   }
 }
