3aa630936abf0d08a64872c454843ea1380badf4
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-28 14:25:25 +0800

    GREEN: deep-clone should deeply clone objects with circular references

--- src/connect-4-domain/deep-clone.ts
+++ src/connect-4-domain/deep-clone.ts
@@ -1,6 +1,9 @@
-function deepClone<T>(value: T): T {
+function deepClone<T>(value: T, visitedCache: WeakMap<any, any> = new WeakMap()): T {
   if (!(value instanceof Object) || typeof value === 'function') {
     return value
+  }
+  if (visitedCache.has(value)) {
+    return visitedCache.get(value)
   }
 
   let result: T
@@ -10,9 +13,12 @@ function deepClone<T>(value: T): T {
   } else {
     result = {} as T
   }
+  visitedCache.set(value, result)
 
   for (const key of Object.keys(value)) {
-    ;(result as any)[key] = deepClone((value as any)[key])
+    const nestedValue = (value as any)[key]
+    const clonedValue = deepClone<typeof nestedValue>(nestedValue, visitedCache)
+    ;(result as any)[key] = clonedValue
   }
 
   return result
