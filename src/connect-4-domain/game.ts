8129b3ae2634d46ed8ced4b8612ab83f8735ecc4
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-28 15:30:15 +0800

    REFACTOR: given custom board dimensions which results in an odd number of cells throws an error

--- src/connect-4-domain/game.ts
+++ src/connect-4-domain/game.ts
@@ -34,6 +34,12 @@ class GameFactory implements Game {
   private players: Record<PlayerNumber, PlayerStats>
 
   constructor({ boardDimensions }: GameParameters = { boardDimensions: { rows: 6, columns: 7 } }) {
+    this.#validateBoardDimensions(boardDimensions)
+    this.board = this.#createBoard(boardDimensions)
+    this.players = this.#createPlayers(boardDimensions)
+  }
+
+  #validateBoardDimensions(boardDimensions: BoardDimensions) {
     if (boardDimensions.rows < 1) {
       throw new InvalidBoardDimensionsError('Number of rows must be greater than or equal to 1')
     } else if (boardDimensions.columns < 1) {
@@ -43,8 +49,6 @@ class GameFactory implements Game {
         `Total number of cells on a board must be even. Supplied board dimensions (${boardDimensions.rows} rows x ${boardDimensions.columns} columns) results in an odd number of cells (${boardDimensions.rows * boardDimensions.columns})`,
       )
     }
-    this.board = this.#createBoard(boardDimensions)
-    this.players = this.#createPlayers(boardDimensions)
   }
 
   getBoard() {
