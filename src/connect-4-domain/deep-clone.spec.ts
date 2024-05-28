1589375dd2b53ab35f7a5ec34dde3d6b74e81d5c
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-28 13:41:44 +0800

    RED: deep-clone should return a deep copy of a function

--- src/connect-4-domain/deep-clone.spec.ts
+++ src/connect-4-domain/deep-clone.spec.ts
@@ -5,17 +5,39 @@ describe('deep-clone', () => {
     expect(deepClone(2)).toBe(2)
   })
   it('should return a deep copy of an array', () => {
-    const original: (object | number[] | number)[] = [{ a: 1 }, 2, [3, 4]]
+    const original: [object, number, number[]] = [{ a: 1 }, 2, [3, 4]]
     const cloned = deepClone(original)
     expect(cloned).not.toBe(original)
     expect(cloned[0]).not.toBe(original[0])
     expect(cloned[0]).toBeInstanceOf(Object)
     expect(Object.keys(cloned[0])).toHaveLength(1)
     expect(Object.hasOwn(cloned[0] as object, 'a')).toBeTruthy()
-    expect((cloned[0] as { a: number }).a).toStrictEqual((original[0] as { a: number }).a)
+    expect((cloned[0] as any)['a']).toStrictEqual((original[0] as { a: number }).a)
     expect(cloned[1]).toBe(original[1])
     expect(cloned[2]).not.toBe(original[2])
-    expect((cloned[2] as number[])[0]).toBe((original[2] as number[])[0])
-    expect((cloned[2] as number[])[1]).toBe((original[2] as number[])[1])
+    expect(cloned[2][0]).toBe(original[2][0])
+    expect(cloned[2][1]).toBe(original[2][1])
+  })
+  it('should return a deep copy of an object', () => {
+    const original: { a: number; b: { c: string; d: number }; e: number[] } = {
+      a: 42,
+      b: { c: 'hi', d: 9 },
+      e: [3, 4],
+    }
+    const cloned = deepClone(original)
+    expect(cloned).not.toBe(original)
+    expect(cloned.a).toStrictEqual(original.a)
+    expect(cloned.b).not.toBe(original.b)
+    expect(cloned.b.c).toStrictEqual(original.b.c)
+    expect(cloned.b.d).toStrictEqual(original.b.d)
+    expect(cloned.e).not.toBe(original.e)
+    expect(cloned.e[0]).toStrictEqual(original.e[0])
+    expect(cloned.e[1]).toStrictEqual(original.e[1])
+  })
+  it('should return a deep copy of a function', () => {
+    const original = (x: number) => 2 * x
+    const cloned = deepClone(original)
+    expect(original(3)).toStrictEqual(cloned(3))
+    expect(original).not.toBe(cloned)
   })
 })
