06e6f68e3de729438e64265117e2c2ee47bcdb1c
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-28 15:36:51 +0800

    RED/GREEN: given custom board dimensions with a negative number of rows it throws an error

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -79,6 +79,13 @@ describe('game', () => {
           )
         })
       })
+      describe('with a negative number of rows', () => {
+        it('throws an error', () => {
+          expect(() => new GameFactory({ boardDimensions: { rows: -3, columns: 3 } })).toThrow(
+            new InvalidBoardDimensionsError('Number of rows must be greater than or equal to 1'),
+          )
+        })
+      })
       describe('which results in an odd number of cells', () => {
         it('throws an error', () => {
           expect(() => new GameFactory({ boardDimensions: { rows: 3, columns: 3 } })).toThrow(
