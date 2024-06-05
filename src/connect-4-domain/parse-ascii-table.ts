eb883548840077a3497fa5561e57370d0e7dcef8
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-05 10:45:02 +0800

    GREEN: given a table with 1 row and 1 column with a non-empty cell and a custom cell-resolver returns a 1x1 grid with a resolved value

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,22 +1,22 @@
-function parseAsciiTable(asciiTable: string): Array<Array<string | undefined>> {
+function parseAsciiTable<T>(
+  asciiTable: string,
+  customResolver: (value: string) => T = (value: string) => value as T,
+): Array<Array<T>> {
   if (asciiTable.length === 0) {
     return []
   }
   const asciiTableRows = asciiTable.split('\n').slice(1)
   const grid = asciiTableRows.reduce(
-    (
-      grid: Array<Array<string | undefined>>,
-      row: string,
-      currentIndex: number,
-    ): Array<Array<string | undefined>> => {
+    (grid: Array<Array<T>>, row: string, currentIndex: number): Array<Array<T>> => {
       if (currentIndex % 2 === 0) {
         return grid
       }
       const rowCells = row.split('|')
       if (rowCells[1].trim().length === 0) {
-        grid.push([undefined])
+        grid.push([undefined as T])
       } else {
-        grid.push([rowCells[currentIndex].trimEnd().slice(1)])
+        const cellContent: string = rowCells[currentIndex].trimEnd().slice(1)
+        grid.push([customResolver(cellContent)])
       }
       return [...grid]
     },
