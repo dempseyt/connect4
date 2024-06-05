4314cc08657ef411aa63ea523e2bcbacc58aec99
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-05 14:14:51 +0800

    RED: given a board and the next players move and the board has 3 rows it does not result in a win

--- src/connect-4-domain/is-winning-move.spec.ts
+++ src/connect-4-domain/is-winning-move.spec.ts
@@ -4,20 +4,20 @@ import isWinningMove from '@/connect-4-domain/is-winning-move'
 import { BoardCell, PlayerMove } from '@/connect-4-domain/game'
 
 describe('is-winning-move', () => {
+  const customResolver = (value: string): BoardCell => {
+    const playerNumber = Number.parseInt(value)
+    if (playerNumber === 1 || playerNumber === 2) {
+      return {
+        player: playerNumber,
+      }
+    }
+    return {
+      player: undefined,
+    }
+  }
   describe('given a board and the next players move', () => {
     describe('results in a vertical win', () => {
       it('detects the win', () => {
-        const customResolver = (value: string): BoardCell => {
-          const playerNumber = Number.parseInt(value)
-          if (playerNumber === 1 || playerNumber === 2) {
-            return {
-              player: playerNumber,
-            }
-          }
-          return {
-            player: undefined,
-          }
-        }
         const asciiTable = `
 |---|---|
 | 1 | 2 |
@@ -41,6 +41,30 @@ describe('is-winning-move', () => {
             isWin: true,
           }),
         )
+      })
+    })
+    describe('and the board has 3 rows', () => {
+      it('does not result in a win', () => {
+        const playerMove = {
+          player: 1,
+          targetCell: {
+            row: 2,
+            column: 0,
+          },
+        } as PlayerMove
+        const asciiTable = `
+|---|
+| 1 |
+|---|
+| 2 |
+|---|
+|   |        
+|---|
+`
+        const board = parseAsciiTable(asciiTable, customResolver)
+        expect(isWinningMove(board, playerMove)).toEqual({
+          isWin: false,
+        })
       })
     })
   })
