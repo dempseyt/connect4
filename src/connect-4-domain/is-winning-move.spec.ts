c7a5abade3750bdcba2e71b2f390217c71c1e5d6
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-05 16:11:51 +0800

    RED: given a board and the next players move > results in a horizontal win > and there are 3 of the active players tokens to the right of the target cell > detects the win

--- src/connect-4-domain/is-winning-move.spec.ts
+++ src/connect-4-domain/is-winning-move.spec.ts
@@ -65,6 +65,25 @@ describe('is-winning-move', () => {
           })
         })
       })
+      describe('and there are 3 of the active players tokens to the right of the target cell', () => {
+        it('detects the win', () => {
+          const asciiTable = `
+|---|---|---|---|
+|   | 1 | 1 | 1 |
+|---|---|---|---|
+|   | 2 | 2 | 2 |
+|---|---|---|---|`
+          const board = parseAsciiTable(asciiTable, customResolver)
+          const playerMove = {
+            player: 1,
+            targetCell: {
+              row: 0,
+              column: 0,
+            },
+          } as PlayerMove
+          expect(isWinningMove(board, playerMove)).toEqual(expect.objectContaining({ isWin: true }))
+        })
+      })
       describe('and there are less than 3 columns to the left of the target cell', () => {
         describe('and there are no player tokens to the right hand side of the target cell', () => {
           it('does not detect a win', () => {
