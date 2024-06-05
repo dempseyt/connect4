009624e601e75597f1d5935d13d27c514980dede
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-05 10:11:23 +0800

    GREEN: given a table with 1 row and 1 column with a non-empty cell returns a 1x1 grid with trailing whitespace

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,16 +1,18 @@
-function parseAsciiTable(asciiTable: string): any[][] {
+function parseAsciiTable<T>(asciiTable: string): Array<Array<T>> {
   if (asciiTable.length === 0) {
     return []
   }
   const asciiTableRows = asciiTable.split('\n').slice(1)
   const grid = asciiTableRows.reduce(
-    (grid: any[][], row: string, currentIndex: number): any[][] => {
+    (grid: Array<Array<T>>, row: string, currentIndex: number): Array<Array<T>> => {
       if (currentIndex % 2 === 0) {
         return grid
       }
       const rowCells = row.split('|')
       if (rowCells[1].trim().length === 0) {
-        grid.push([undefined])
+        grid.push([undefined as T])
+      } else {
+        grid.push([rowCells[currentIndex].trim()])
       }
       return [...grid]
     },
