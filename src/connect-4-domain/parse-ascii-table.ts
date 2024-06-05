4847caa0030f1032242c17555590ad91efb31407
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-05 10:18:27 +0800

    REFACTOR: given a table with 1 row and 1 column with a non-empty cell returns a 1x1 grid with trailing whitespace

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,16 +1,20 @@
-function parseAsciiTable<T>(asciiTable: string): Array<Array<T>> {
+function parseAsciiTable(asciiTable: string): Array<Array<string | undefined>> {
   if (asciiTable.length === 0) {
     return []
   }
   const asciiTableRows = asciiTable.split('\n').slice(1)
   const grid = asciiTableRows.reduce(
-    (grid: Array<Array<T>>, row: string, currentIndex: number): Array<Array<T>> => {
+    (
+      grid: Array<Array<string | undefined>>,
+      row: string,
+      currentIndex: number,
+    ): Array<Array<string | undefined>> => {
       if (currentIndex % 2 === 0) {
         return grid
       }
       const rowCells = row.split('|')
       if (rowCells[1].trim().length === 0) {
-        grid.push([undefined as T])
+        grid.push([undefined])
       } else {
         grid.push([rowCells[currentIndex].trim()])
       }
