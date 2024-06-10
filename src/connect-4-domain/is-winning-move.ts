2b08293b718a03ea2012917e25d855b4dd1ac937
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-10 14:36:37 +0800

    REFACTOR: given a board and the next players move > results in a horizontal win > and there are 2 of the moving players tokens to the left and 1 to the right of the target cell > detects the win

--- src/connect-4-domain/is-winning-move.ts
+++ src/connect-4-domain/is-winning-move.ts
@@ -1,75 +1,34 @@
 import { Board, BoardCell, PlayerMove } from './game'
 
-function getTargetCell(playerMove: PlayerMove): { targetRow: number; targetColumn: number } {
-  return {
-    targetRow: playerMove.targetCell.row,
-    targetColumn: playerMove.targetCell.column,
-  }
+function getThreeDisksToLeftOfTargetCell(board: Board, playerMove: PlayerMove): Array<BoardCell> {
+  const columnIndex = playerMove.targetCell.column
+  const leftStartIndex = Math.max(columnIndex - 3, 0)
+  return board[playerMove.targetCell.row].slice(leftStartIndex, columnIndex)
 }
 
-function getThreeDisksToLeftOfTargetCell(
-  board: Board,
-  playerMove: PlayerMove,
-): Array<number | undefined> {
-  const { targetRow, targetColumn } = getTargetCell(playerMove)
-  const distanceFromLeft = playerMove.targetCell.column
-  switch (distanceFromLeft) {
-    case 0: {
-      return []
-    }
-    case 1:
-      return [board[targetRow][targetColumn - 1].player]
-    case 2:
-      return [board[targetRow][targetColumn - 1].player, board[targetRow][targetColumn - 2].player]
-    default:
-      return [
-        board[targetRow][targetColumn - 1].player,
-        board[targetRow][targetColumn - 2].player,
-        board[targetRow][targetColumn - 3].player,
-      ]
-  }
+function getThreeDisksToRightOfTargetCell(board: Board, playerMove: PlayerMove): Array<BoardCell> {
+  const columnIndex = playerMove.targetCell.column
+  const rightEndIndex = Math.min(board[0].length, columnIndex + 3)
+  return board[playerMove.targetCell.row].slice(columnIndex + 1, rightEndIndex)
 }
 
-function getThreeDisksToRightOfTargetCell(
-  board: Board,
-  playerMove: PlayerMove,
-): Array<number | undefined> {
-  const { targetRow, targetColumn } = getTargetCell(playerMove)
-  const onTheEdgeOfBoard = board[0].length - 1
-  const oneFromTheEdgeOfBoard = board[0].length - 2
-  const twoFromTheEdgeOfBoard = board[0].length - 3
-  if (targetColumn === onTheEdgeOfBoard) {
-    return []
-  } else if (targetColumn === oneFromTheEdgeOfBoard) {
-    return [board[targetRow][targetColumn + 1].player]
-  } else if (targetColumn === twoFromTheEdgeOfBoard) {
-    return [board[targetRow][targetColumn + 1].player, board[targetRow][targetColumn + 2].player]
-  } else {
-    return [
-      board[targetRow][targetColumn + 1].player,
-      board[targetRow][targetColumn + 2].player,
-      board[targetRow][targetColumn + 3].player,
-    ]
-  }
+function getIsCellOccupiedByPlayer(currentCell: BoardCell, activePlayer: 1 | 2): boolean {
+  return currentCell.player === activePlayer
 }
 
 function isHorizontalWin(board: Board, playerMove: PlayerMove): { isWin: boolean } {
   if (board[0].length < 3) {
     return { isWin: false }
   }
-  const player = playerMove.player
-  const threeDisksToLeftAndRightOfTargetCell: Array<number | undefined> = [
+  const accumulatedCellsAroundTargetCell: Array<BoardCell> = [
     ...getThreeDisksToLeftOfTargetCell(board, playerMove),
     ...getThreeDisksToRightOfTargetCell(board, playerMove),
   ]
+  const activePlayer = playerMove.player
   let count = 0
-  const isWin = threeDisksToLeftAndRightOfTargetCell.reduce(
-    (isWinningMove: boolean, currentCell: number | undefined): boolean => {
-      if (currentCell === player) {
-        count++
-      } else {
-        count = 0
-      }
+  const isWin = accumulatedCellsAroundTargetCell.reduce(
+    (isWinningMove: boolean, currentCell: BoardCell): boolean => {
+      getIsCellOccupiedByPlayer(currentCell, activePlayer) ? count++ : (count = 0)
       if (count === 3) return isWinningMove
       return !isWinningMove
     },
