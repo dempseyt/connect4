f8d370eed8b58ddfac396d5c4016b4e008617cc5
Dempey Thompson <dempsey.thompson@journeyone.com.au>
2024-06-12 16:23:04 +0800

    REFACTOR: getting the status of the game and player two has won reports the status as player two win

--- src/connect-4-domain/game.ts
+++ src/connect-4-domain/game.ts
@@ -171,11 +171,7 @@ class GameFactory implements Game {
   }: MovePlayerCommand): PlayerMovedEvent {
     const isWinningMove = getIsWinningMove(this.getBoard(), { player, targetCell: { row, column } })
     if (isWinningMove) {
-      if (player === 1) {
-        this.status = Status.PLAYER_ONE_WIN
-      } else if (player === 2) {
-        this.status = Status.PLAYER_TWO_WIN
-      }
+      this.status = player === 1 ? Status.PLAYER_ONE_WIN : Status.PLAYER_TWO_WIN
     }
     this.board[row][column] = { player }
     this.activePlayer = this.activePlayer === 1 ? 2 : 1
