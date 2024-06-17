939e3d86743385f654a710bff6742a3cf6ba68d4
dempseyt <dempsey.thompson@journeyone.com.au>
2024-06-17 16:45:11 +0800

    GREEN: in-memory-repository > given a store > saves a board

--- src/connect-4-domain/in-memory-repository.ts
+++ src/connect-4-domain/in-memory-repository.ts
@@ -1,5 +1,25 @@
-interface GameRepository {}
+import { Board } from './game'
 
-class InMemoryRepository implements GameRepository {}
+type BoardUuid = `${string}-${string}-${string}-${string}-${string}`
+
+type Store = Map<BoardUuid, Board>
+
+interface GameRepository {
+  save: (board: Board) => BoardUuid
+}
+
+class InMemoryRepository implements GameRepository {
+  private store: Store
+
+  constructor(store: Store = new Map<BoardUuid, Board>()) {
+    this.store = store
+  }
+
+  save(board: Board): BoardUuid {
+    const boardId = crypto.randomUUID()
+    this.store.set(boardId, board)
+    return boardId
+  }
+}
 
 export default InMemoryRepository
